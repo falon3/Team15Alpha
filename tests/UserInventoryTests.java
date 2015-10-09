6b5127f8ed97b26478275b571a0704088f12ea78
Noah Weninger <nweninge@ualberta.ca>
2015-10-08 21:20:27 -0600

    finish (probably) user inventory tests

--- tests/UserInventoryTests.java
+++ tests/UserInventoryTests.java
@@ -2,11 +2,12 @@ public class UserInventoryTests {
   public void testSkillManipulation() {
     Inventory inv = new Inventory();
     Skill skill = new Skill("Skill Name", "category");
-    
+
     // Testing adding a skill to inventory
     inv.add(skill);
+	// test for getting skill from inventory as well
     assertTrue(inv.get(0).equals(skill));
-    
+
     // Testing modifying a skill in inventory
     Photo dog = new Photo("dog chasing it's tail");
     Skill held_skill = inv.get(0);
@@ -16,11 +17,35 @@ public class UserInventoryTests {
     assertTrue(dog.equals(held_skill.getPhoto()));
     assertTrue(held_skill.getDescription().equals("I jumped and then got really tired"));
     assertTrue(held_skill.getVisibility().equals(false));
-    
+
     // Testing removing a skill from inventory
     inv.remove(skill);
     assertTrue(inv.size()==0);
-    
-    
+
+
+  }
+  public void testSkillSorting() {
+    Inventory inv = new Inventory();
+    Skill skill = new Skill("Skill Name", "category");
+    Skill skill2 = new Skill("Skill Naem", "dategory");
+
+	inv.add(skill2);
+	inv.add(skill);
+
+	assertEquals(inv.getSkillsSortedByCategory(), new ArrayList<Skill> { skill, skill2 });
+  }
+  public void testSearchSkills() {
+    Inventory inv = new Inventory();
+    Skill skill = new Skill("Skill Name", "category");
+    Skill skill2 = new Skill("Skil Naem", "dategory");
+
+	inv.add(skill2);
+	inv.add(skill);
+
+	assertEquals(inv.findByName("Name"), new Set<Skill> { skill });
+	assertEquals(inv.findByName("Naem"), new Set<Skill> { skill2 });
+	assertEquals(inv.findByCategory("category"), new Set<Skill> { skill });
+	assertEquals(inv.findByCategory("dategory"), new Set<Skill> { skill2 });
+	assertEquals(inv.findByCategory("gory"), new Set<Skill> { skill1, skill2 });
   }
 }
