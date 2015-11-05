0adf2d2d727c9e06c80ebd1e9ab2a8502a573ece
Falon <scheers@ualberta.ca>
2015-11-04 21:44:10 -0700

    get up to date stufff not done Local yet

---
+++ Skill/app/src/main/java/com/skilltradiez/skilltraderz/Local.java
@@ -0,0 +1,16 @@
+package com.skilltradiez.skilltraderz;
+
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Falon3 on 2015-11-04.
+ */
+public class Local {
+    private static final String FILENAME = "file.sav";
+    
+
+}
