80bf7b2a47c58934497d8bbfa7910af270acdf10
Falon <scheers@ualberta.ca>
2015-11-04 23:47:28 -0700

    Falon Finally did something that should work!

---
+++ Skill/app/src/main/java/com/skilltradiez/skilltraderz/LocalPersistentObject.java
@@ -0,0 +1,55 @@
+package com.skilltradiez.skilltraderz;
+/*
+ *    Team15Alpha
+ *    AppName: SkillTradiez (Subject to change)
+ *    Copyright (C) 2015  Stephen Andersen, Falon Scheers, Elyse Hill, Noah Weninger, Cole Evans
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+import java.util.List;
+
+/**
+ * Created by Falon3 on 2015-11-04.
+ */
+public class LocalPersistentObject {
+    private User currentUser;
+    private List<User> users;
+    private List<Notification> notifications;
+
+    public User getCurrentUser() {
+        return currentUser;
+    }
+
+    public void setCurrentUser(User currentUser) {
+        this.currentUser = currentUser;
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
+
+    public List<Notification> getNotifications() {
+        return notifications;
+    }
+
+    public void setNotifications(List<Notification> notifications) {
+        this.notifications = notifications;
+    }
+
+
+}
