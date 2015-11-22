925dd3f409c1971ced8466a24bdfab2126e7b016
Stephen <sja2@ualberta.ca>
2015-11-21 20:25:42 -0700

    SearchScreenUI Testing Has Begun

---
+++ Skill/app/src/androidTest/java/com/skilltradiez/skilltraderz/SearchScreenUITests.java
@@ -0,0 +1,48 @@
+package com.skilltradiez.skilltraderz;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+/**
+ * Created by Stephen on 2015-11-21.
+ */
+public class SearchScreenUITests extends ActivityInstrumentationTestCase2 {
+    SearchScreenUITests() {
+        super(com.skilltradiez.skilltraderz.SearchScreenActivity.class);
+    }
+
+    public void testUsers() {
+        //Give it Users
+    }
+
+    public void testSearchUsers() {
+        //Search for Godfrey
+    }
+
+    public void testCategorySearchUsers() {
+        //Friends
+    }
+
+    public void testSkillz() {
+        //Give it Skillz
+    }
+
+    public void testSearchSkillz() {
+        //Search for Ability To Survive Long Falls
+    }
+
+    public void testCategorySearchSkillz() {
+        //Physical, Stealth, Science, Math, Whatever, ..., Misc
+    }
+
+    public void testTrades() {
+        //Give it Trade
+    }
+
+    public void testSearchTrades() {
+        //Search for that violin teacher
+    }
+
+    public void testCategorySearchTrades() {
+        //Active, Inactive
+    }
+}
