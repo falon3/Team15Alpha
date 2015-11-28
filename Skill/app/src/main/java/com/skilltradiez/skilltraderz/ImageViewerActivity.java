de57d60a8f32ec1b2b27847cc1bd2809584ccf54
Noah Weninger <nweninge@ualberta.ca>
2015-11-28 16:06:24 -0700

    ImageView activity

---
+++ Skill/app/src/main/java/com/skilltradiez/skilltraderz/ImageViewerActivity.java
@@ -0,0 +1,23 @@
+package com.skilltradiez.skilltraderz;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.widget.ImageView;
+
+public class ImageViewerActivity extends ActionBarActivity {
+    static String IMAGE_ID_PARAM = "image_id";
+
+    private ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image_viewer);
+
+        imageView = (ImageView) findViewById(R.id.imageView);
+
+        Image image = DatabaseController.getImageByID((ID) getIntent().getExtras().get(IMAGE_ID_PARAM));
+        imageView.setImageBitmap(image.getBitmap());
+    }
+
+}
