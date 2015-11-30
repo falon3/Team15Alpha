8e1f6f86820a00893cbea9ea24e668239fe56f17
Falon <scheers@ualberta.ca>
2015-11-29 20:36:28 -0700

    All UI tests pass right now whoohoo!

--- Skill/app/src/androidTest/java/com/skilltradiez/skilltraderz/TestUtilities.java
+++ Skill/app/src/androidTest/java/com/skilltradiez/skilltraderz/TestUtilities.java
@@ -25,13 +25,18 @@ import java.util.Random;
 public class TestUtilities {
     private static Random random = new Random();
     private static String[] components = new String[] {
-            "aeo", "rom", "ina", "lao", "nye", "yso", "poa", "oem", "yula"
+            ""
     };
     public static String getRandomString() {
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 5; i++) {
             sb.append(components[random.nextInt(components.length)]);
         }
-        return sb.toString();
+        sb.append(random.nextInt());
+        String result = sb.toString();
+        if (result.length() > 9) {
+            result= result.substring(0,9);
+        }
+        return result;
     }
 }
