7f819f8a7a8486676761239435debf02e7e6d4b1
Noah Weninger <nweninge@ualberta.ca>
2015-11-30 13:43:35 -0700

    Fix counter trades

--- Skill/app/src/main/java/com/skilltradiez/skilltraderz/EditTradeActivity.java
+++ Skill/app/src/main/java/com/skilltradiez/skilltraderz/EditTradeActivity.java
@@ -84,7 +84,6 @@ public class EditTradeActivity extends ButtonMenuActivity {
     private ListView offerList, yourInvList, requestList, otherInvList;
 
     private TextView tradeTitle, otherInvTitle;
-    private Button sendTrade, cancelTrade;
 
     private ArrayList<Skill> offer, yourInv, request, otherInv;
     private User activeUser, passiveUser;
@@ -115,9 +114,6 @@ public class EditTradeActivity extends ButtonMenuActivity {
         otherInv = new ArrayList<Skill>();
 
         // Get UI elements
-        sendTrade = (Button) findViewById(R.id.sendTrade);
-        cancelTrade = (Button) findViewById(R.id.deleteTrade);
-
         tradeTitle = (TextView) findViewById(R.id.trading_with);
         otherInvTitle = (TextView) findViewById(R.id.other_inv);
 
@@ -273,6 +269,7 @@ public class EditTradeActivity extends ButtonMenuActivity {
         }
         if (trade != null){
             trade.set(masterController.getUserDB(), activeUser, passiveUser, offer, request);
+            DatabaseController.addTrade(trade);
         } else {
             trade = activeUser.getTradeList().createTrade(masterController.getUserDB(), activeUser, passiveUser, offer, request);
         }
