016ac9d5c8de9906d6657ea3cdf5796bfedcafa1
Noah Weninger <nweninge@ualberta.ca>
2015-11-30 16:33:25 -0700

    fix for demo

--- Skill/app/src/main/java/com/skilltradiez/skilltraderz/UserDatabase.java
+++ Skill/app/src/main/java/com/skilltradiez/skilltraderz/UserDatabase.java
@@ -97,7 +97,7 @@ public class UserDatabase {
      * @param client HTTPClient Object
      */
     public void setHttpClient(HTTPClient client){
-        elastic = new Elastic("http://cmput301.softwareprocess.es:8080/cmput301f15t15/", client);
+        elastic = new Elastic("http://169.254.113.89:9200/cmput301f15t15/", client);
     }
 
     /**
