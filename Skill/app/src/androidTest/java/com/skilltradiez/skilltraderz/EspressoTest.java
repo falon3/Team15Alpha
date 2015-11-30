6d606271443b9e70c9b2e3a2efa3bc9012ec2caf
Falon <scheers@ualberta.ca>
2015-11-30 02:35:46 -0700

    more ui tests

--- Skill/app/src/androidTest/java/com/skilltradiez/skilltraderz/EspressoTest.java
+++ Skill/app/src/androidTest/java/com/skilltradiez/skilltraderz/EspressoTest.java
@@ -72,8 +72,10 @@ public class EspressoTest {
         MasterController.getUserDB().getLocal().deleteFile();
     }
 
-    @Test
+
     // Login.CreateAccount
+
+    @Test
     public void testCreateUser() {
         String username = TestUtilities.getRandomString();
         String email = TestUtilities.getRandomString();
@@ -84,12 +86,33 @@ public class EspressoTest {
         onView(withId(R.id.Go_Profile_Menu)).perform(click());
         onView(withId(R.id.user_name)).check(matches(withText(username)));
     }
+    @Test
+    public void testModifySkillDetails(){
+        //create user and add skill
+        testAddSkillz();
+
+        // now go to inventory and edit that skill
+        onView(withId(R.id.Go_Profile_Menu)).perform(click());
+        onView(withId(R.id.inventory)).perform(click());
+        // click on the skill click to edit
+        onData(anything()).inAdapterView(withId(R.id.results_list)).atPosition(0).perform(click());
+        onView(withId(R.id.edit_skill)).perform(click());
+
+        //edit skill description
+        onView(withId(R.id.new_skill_description)).perform(typeText("Edited Skill description"), closeSoftKeyboard());
+        onView(withId(R.id.add_skill_to_database)).perform(click());
+
+        // go back to inventory and assert that the stuff is set current
+        onView(withId(R.id.skill_description)).check(matches(withText("Edited Skill description")));
+    }
 
     // Inventory.AddSkillz + ExamineSkillz (examines after adding)
+    public String username;
+    public String email;
     @Test
     public void testAddSkillz() {
-        String username = "Drew" + TestUtilities.getRandomString();
-        String email = "E" + TestUtilities.getRandomString();
+        username = "Drew" + TestUtilities.getRandomString();
+        email = "E" + TestUtilities.getRandomString();
         //login
         onView(withId(R.id.usernameField)).perform(typeText(username), closeSoftKeyboard());
         onView(withId(R.id.emailField)).perform(typeText(email), closeSoftKeyboard());
@@ -102,8 +125,13 @@ public class EspressoTest {
         onView(withId(R.id.new_skill_name)).perform(typeText("Sname"), closeSoftKeyboard());
         onView(withId(R.id.new_skill_description)).perform(typeText("Sdesc"), closeSoftKeyboard());
         onView(withId(R.id.radioButton)).perform(click());
-        // Set visibility
+        // Set visibility (default is visible
         //onView(withId(R.id.is_visible)).perform(click());
+
+        //Not sure how to set category spinner with espresso..... but it works in the app if you try it yourself
+        //onView(withId(R.id.category_spinner)).perform(click());
+        //set selection of category spinner somehow
+
 
         // add skill to db
         onView(withId(R.id.add_skill_to_database)).perform(click());
@@ -187,19 +215,151 @@ public class EspressoTest {
     }
 
     @Test
+    public void testBrowseOwnInventory() throws UserAlreadyExistsException {
+        //create user and add skill
+        testAddSkillz();
+
+        // go back to home screen and then to profile and inventory
+        onView(withId(R.id.Go_Home_Menu)).perform(click());
+        onView(withId(R.id.Go_Profile_Menu)).perform(click());
+        onView(withId(R.id.inventory)).perform(click());
+
+        // now browsing inventory... can click on a skill and read full details
+        onData(anything()).inAdapterView(withId(R.id.results_list)).atPosition(0).perform(click());
+
+        // assert that the stuff is set current
+        onView(withId(R.id.skillTitle)).check(matches(withText("Sname")));
+        onView(withId(R.id.skill_description)).check(matches(withText("Sdesc")));
+    }
+
+    public void editSkillVisibility(){
+        //create user and add skill
+        testAddSkillz();
+
+        // go back to home screen and then to profile and inventory
+        onView(withId(R.id.Go_Home_Menu)).perform(click());
+        onView(withId(R.id.Go_Profile_Menu)).perform(click());
+        onView(withId(R.id.inventory)).perform(click());
+
+        // now browsing inventory... can click on a skill and read full details
+        onData(anything()).inAdapterView(withId(R.id.results_list)).atPosition(0).perform(click());
+
+        // edit visibility
+        onView(withId(R.id.edit_skill)).perform(click());
+        onView(withId(R.id.is_visible)).perform(click());
+        onView(withId(R.id.add_skill_to_database)).perform(click());
+
+        // go back to browse all skills and assert that it is not there
+        onView(withId(R.id.Go_Home_Menu)).perform(click());
+        onView(withId(R.id.All_Skillz)).perform(click());
+
+        //search for the skill that shouldn't be there
+        onView(withId(R.id.search_bar)).perform(typeText("Sname"), closeSoftKeyboard());
+        onView(withId(R.id.search_bar_button)).perform(click());
+
+        // Not sure how espresso really works, this seems like a hack but it works.
+        // if an exception is not thrown getting the first item, then there was an item and
+        // therefore it wasn't removed from visibility successfully.
+        try {
+            onData(anything()).inAdapterView(allOf(withId(R.id.results_list), isDisplayed())).atPosition(0).check(matches(isDisplayed()));
+        } catch (Exception e) {
+            return;
+        }
+        assertTrue(false);
+    }
+
+    @Test
     public void testBrowseFriendInventory() throws UserAlreadyExistsException {
         //add friend first
         testAddFriend();
 
-        // Check profile details
+        // Check profile details and click inventory
         onView(withId(R.id.user_name)).check(matches(withText(friendAddfriend)));
+        onView(withId(R.id.inventory)).perform(click());
+        // now browsing inventory
+    }
 
+    @Test
+    public void testBrowseFriendProfile() throws UserAlreadyExistsException {
+        // create account login and add a friend
+        testAddFriend();
+
+        // now browse users and can search and select friend's profile
+        onView(withId(R.id.Go_Home_Menu)).perform(click());
+        onView(withId(R.id.All_Users)).perform(click());
+        onView(withId(R.id.search_bar)).perform(typeText(friendAddfriend), closeSoftKeyboard());
+        onView(withId(R.id.search_bar_button)).perform(click());
+        onData(anything()).inAdapterView(withId(R.id.results_list)).atPosition(0).perform(click());
+
+        //check we are now browsing our friend's profile
+        onView(withId(R.id.user_name)).check(matches(withText(friendAddfriend)));
+    }
+
+    @Test
+    public void sortSkillsByCategory(){
+        // This works you can see by trying it.... but how to navigate a category spinner UI test with
+        // Espresso is not clear and Google has failed me....
     }
     @Test
-    public void testStartTrade() throws UserAlreadyExistsException {
+    public void sortSkillsByText(){
+        // create a user and login and create a skill
+        testAddSkillz();
+
+        // go back to browse all skills and assert that it is not there
+        onView(withId(R.id.Go_Home_Menu)).perform(click());
+        onView(withId(R.id.All_Skillz)).perform(click());
+
+        //search for the skill that should be there
+        onView(withId(R.id.search_bar)).perform(typeText("Sname"), closeSoftKeyboard());
+        onView(withId(R.id.search_bar_button)).perform(click());
+
+        //ensure that our skill was found after sorting by text
+        onData(anything()).inAdapterView(allOf(withId(R.id.results_list), isDisplayed())).atPosition(0).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void sortFriendInventoryByText() throws UserAlreadyExistsException {
+        // create a user and login and add friend
         String tradeFriend = "Al" + TestUtilities.getRandomString();
         String tradeUsername = "Bo" + TestUtilities.getRandomString();
         String tradeEmail = "Em" + TestUtilities.getRandomString();
+        //create friend, make sure they have a skill, add them
+        DatabaseController.createUser(tradeFriend);
+        GeneralMenuActivity g_activity = (GeneralMenuActivity) getActivityInstance();
+        g_activity.getMasterController().makeNewSkill("miscellaneous", "stupid sort test", "High", true, new ArrayList<Image>());
+
+        //login and add friend
+        onView(withId(R.id.usernameField)).perform(typeText(tradeUsername), closeSoftKeyboard());
+        onView(withId(R.id.emailField)).perform(typeText(tradeEmail), closeSoftKeyboard());
+        onView(withId(R.id.beginApp)).perform(click());
+
+        //return home to browse users find friend
+        onView(withId(R.id.Go_Home_Menu)).perform(click());
+        onView(withId(R.id.All_Users)).perform(click());
+        onView(withId(R.id.search_bar)).perform(typeText(tradeFriend), closeSoftKeyboard());
+        onView(withId(R.id.search_bar_button)).perform(click());
+        onData(anything()).inAdapterView(withId(R.id.results_list)).atPosition(0).perform(click());
+
+        //add friend
+        onView(withId(R.id.right_button)).perform(click());
+        onView(withId(R.id.inventory)).perform(click());
+        // sort their inventory by text for the skill we know they have
+        onView(withId(R.id.search_bar)).perform(typeText("stupid sort test"), closeSoftKeyboard());
+        onView(withId(R.id.search_bar_button)).perform(click());
+        //ensure that the skill was found after sorting by text
+        onData(anything()).inAdapterView(allOf(withId(R.id.results_list), isDisplayed())).atPosition(0).check(matches(isDisplayed()));
+    }
+    public String tradeFriend;
+    public String tradeUsername;
+    public String tradeEmail;
+    /* this test ALSO is a test for Notified of Trade Request since we check the trade updates page to see
+    if there is any new trade activity after trade started
+    */
+    @Test
+    public void testStartTrade() throws UserAlreadyExistsException {
+        tradeFriend = "Al" + TestUtilities.getRandomString();
+        tradeUsername = "Bo" + TestUtilities.getRandomString();
+        tradeEmail = "Em" + TestUtilities.getRandomString();
         //create friend, make sure they have a skill, add them
         DatabaseController.createUser(tradeFriend);
         GeneralMenuActivity g_activity = (GeneralMenuActivity) getActivityInstance();
@@ -224,17 +384,17 @@ public class EspressoTest {
         onView(withId(R.id.left_button)).perform(click());
         onData(anything()).inAdapterView(withId(R.id.other_invList)).atPosition(0).perform(click());
         onView(withId(R.id.sendTrade)).perform(click());
+        // toast made that trade request was sent (to be pushed when online if not)
 
         //return home and view trade history
         onView(withId(R.id.Go_Home_Menu)).perform(click());
         onView(withId(R.id.Trade_History)).perform(click());
         onData(anything()).inAdapterView(withId(R.id.results_list)).atPosition(0).perform(click());
 
-        // check that a trade was added
+        // check that a trade was added in trade activity notifications page
         onData(anything()).inAdapterView(allOf(withId(R.id.requestList), isDisplayed())).atPosition(0).check(matches(isDisplayed()));
 
     }
-
     Activity currentActivity;
     // helper function for testStartTrade
     public Activity getActivityInstance(){
